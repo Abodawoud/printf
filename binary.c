4ca0b2f42862a0cdd56f4a271d3e16d84e843d2b
abeermosaad <abeermosaad69@gmail.com>
2023-04-25 18:07:22 +0200

    binary

---
+++ binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * binary - .
+ * @arg: .
+ * Return: on success, .
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+int binary(va_list arg)
+{
+	int n = va_arg(arg, int), len, i;
+	unsigned int m, temp;
+	char *s;
+
+	i = 0;
+	m = n;
+	temp = n;
+	len = 0;
+	while (m)
+	{
+		len++;
+		m /= 2;
+	}
+	s = malloc(len);
+	while (temp)
+	{
+		s[i] = (temp % 2) + '0';
+		i++;
+		temp /= 2;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		write(1, &s[i], 1);
+	}
+	return (len);
+}
