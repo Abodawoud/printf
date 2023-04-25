3f50d1d944e6dae8372fef9e0cd19bb79cbd7a2e
Abodawoud <yousefabodawoud@gmail.com>
2023-04-25 04:55:04 +0200

    test

--- rev.c
+++ rev.c
@@ -12,13 +12,13 @@ int reverse(va_list arg)
 
 	if (s == NULL)
 		return (0);
-		for (i = 0; i < ((int)strlen(s)) / 2; i++)
-		{
-			temp = s[i];
-			s[i] = s[(int)strlen(s) - 1 - i];
-			s[(int)strlen(s) - 1 - i] = temp;
-		}
-		for (i = 0; i < (int)strlen(s); i++)
-			write(1, &s[i], 1);
+	for (i = 0; i < ((int)strlen(s)) / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[(int)strlen(s) - 1 - i];
+		s[(int)strlen(s) - 1 - i] = temp;
+	}
+	for (i = 0; i < (int)strlen(s); i++)
+		write(1, &s[i], 1);
 	return ((int)strlen(s));
 }
